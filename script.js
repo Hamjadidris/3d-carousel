6a1b625bc38929fd23857dd27038b2e2cc0f37c8
Hamjad Idris <dajmahsirdi@gmail.com>
2024-07-04 18:21:44 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const sliderContainer = document.querySelector(".sliders-container");
+const sliders = document.querySelector(".sliders");
+
+let mouseDown = false;
+
+const mousePos = { x: 0, y: 0 };
+const lastSeen = { x: 0, y: 0 };
+
+let id = null;
+clearInterval(id);
+
+setTimeout(function () {
+  id = setInterval(frame, 35);
+}, 3000);
+
+function frame() {
+  mousePos.x += 0.5;
+  sliders.style.transform = `translateX(-50%) perspective(1000px) rotateX(-10deg) rotateY(${mousePos.x}deg)`;
+}
+
+sliderContainer.addEventListener("mousedown", (e) => {
+  mouseDown = true;
+  clearInterval(id);
+});
+
+sliderContainer.addEventListener("mouseup", (e) => {
+  mouseDown = false;
+  id = setInterval(frame, 35);
+});
+
+sliderContainer.addEventListener("mousemove", (e) => {
+  const elem = e.target;
+  if (mouseDown) onMove(e, elem);
+});
+
+sliderContainer.addEventListener("touchmove", (e) => {
+  const elem = e.target;
+  onMove(e.targetTouches[0], elem);
+});
+
+sliderContainer.addEventListener("touchstart", (e) => {
+  clearInterval(id);
+});
+
+sliderContainer.addEventListener("touchend", (e) => {
+  id = setInterval(frame, 35);
+});
+
+function onMove(e, elem) {
+  const mouse_x = e.clientX;
+  const mouse_y = e.clientY;
+  //   console.log(mouse_x, mouse_y);
+  if (lastSeen.y !== mouse_y) {
+    mousePos.y += 1;
+  }
+  if (lastSeen.x !== mouse_x) {
+    mousePos.x += 1;
+  }
+
+  lastSeen.y = mouse_y;
+  lastSeen.x = mouse_x;
+
+  sliders.style.transform = `translateX(-50%) perspective(1000px) rotateX(-10deg) rotateY(${mousePos.x}deg)`;
+
+  //   sliders.style.transform = `rotateY(${mousePos.x}deg) translateX(-50%) perspective(1000px) rotateX(-10deg)`;
+}
